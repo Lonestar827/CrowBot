d980d6425f253d0e0a37c9af007e967cc642d4c1
Lonestar827 <jm_callero@hotmail.com>
2018-02-06 13:10:58 -0800

    Database Overhaul
    Changing how the databases are stored within the bot while its active.

--- SharonBot/BotDocs/SharonBot.py
+++ SharonBot/BotDocs/SharonBot.py
@@ -11,6 +11,20 @@ invoker = 'dandyism'
 stanza1= [line.rstrip('\n') for line in open('FirstDandyStanzas.txt')]
 stanza2= [line.rstrip('\n') for line in open('SecondDandyStanzas.txt')]
 stanza3= [line.rstrip('\n') for line in open('ThirdDandyStanzas.txt')]
+#submissions of stanzas by user input
+submit1= [line.rstrip('\n') for line in open('FirstDandySubmissions.txt')]
+submit2= [line.rstrip('\n') for line in open('SecondDandySubmissions.txt')]
+submit3= [line.rstrip('\n') for line in open('ThirdDandySubmissions.txt')]
+#databases for future use
+stanzaDatabase = [stanza1, stanza2, stanza3]
+submissionDatabase = [submit1, submit2, submit3]
+totalDatabase = [stanzaDatabase, submissionDatabase]
+#database references for !view
+FIRST_STANZA = 0
+SECOND_STANZA = 1
+THIRD_STANZA = 2
+STANZA_DATABASE = 0
+SUBMISSION_DATABASE = 1
 
 @client.event
 async def on_message(message):
@@ -32,9 +46,9 @@ async def on_message(message):
     #!toTheStars : be treated to a dandy haiku
     if message.content.startswith(invoker + '!toTheStars'):
         #generate the stanzas
-            part1 = stanza1[randint(0,len(stanza1))]
-            part2 = stanza2[randint(0,len(stanza2))]
-            part3 = stanza3[randint(0,len(stanza3))]
+            part1 = stanzaDatabase[0][randint(0,len(stanza1))]
+            part2 = stanzaDatabase[1][randint(0,len(stanza2))]
+            part3 = stanzaDatabase[2][randint(0,len(stanza3))]
 
         #put together the messege
             msg = "```" + part1 + "\n" + part2 + "\n" + part3 + "```".format(message)
@@ -79,14 +93,11 @@ async def on_message(message):
 
                 #write to file
                 if(number.content == '1'):
-                    with open('FirstDandySubmitions.txt', 'a') as the_file:
-                                the_file.write(msg.content + '\n')
+                    submit1.append(msg.content)
                 if(number.content == '2'):
-                    with open('SecondDandySubmitions.txt', 'a') as the_file:
-                                the_file.write(msg.content + '\n')
+                    submit2.append(msg.content)
                 if (number.content == '3'):
-                    with open('ThirdDandySubmitions.txt', 'a') as the_file:
-                                the_file.write(msg.content + '\n')    
+                    submit3.append(msg.content)
 
         #not proper input
         else:
@@ -101,7 +112,26 @@ async def on_message(message):
                                  )
 
     #!view : views available stanzas
-    #placeholder text for test
+    #if message.content.startswith(invoker + '!view'):
+        #check for user to be bot owner
+            #if user == bot owner
+        #ask to see which database to look at
+            #send_message("Do you want to see the ``stanzas`` or the ``submissions``?
+                #if the reply is "stanzas"
+                    #database = 1 = STANZA_DATABASE
+                #if the reply is "submissions"
+                    #database = 2 = SUBMISSION_DATABASE
+        #ask to see which stanza they want to see
+            #if the reply is 1
+                #stanza = 0 = FIRST_STANZA
+            #if the reply is 2
+                #stanza = 1 = SECOND_STANZA
+            #if the reply is 3
+                #stanza = 2 = THIRD_STANZA
+        #Generate the list for the message
+            #for i = 0, i < database.length, i++
+            #line = i+1 + stanza\n
+        #
 
     #!invite : invite this bot to other servers
     if message.content.startswith(invoker + '!invite'):
@@ -115,9 +145,10 @@ async def on_message(message):
         "```Credits:\nLonestar : creator and coder for the bot\nSangmillion: Provided the slayer soundfiles for transcription\nUbuyo: Helping with the transcription```"
                                  )
     
-    #!exit : logs_out
+    #!exit : logs_out and updates databases
     if message.content.startswith(invoker + '!exit'):
         if message.author.id == '64917161432322048':
+            #placeholder code for database saving
             msg = 'By your command...'.format(message)
             await client.send_message(message.channel, msg)
             print('logged out')
